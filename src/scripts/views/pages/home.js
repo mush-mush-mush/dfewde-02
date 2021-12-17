87affba2fb929636e90603f70672b05371f126e9
mush-mush-mush <shroom.mish12@gmail.com>
2021-12-18 07:20:45 +0800

    fix CLS

--- src/scripts/views/pages/home.js
+++ src/scripts/views/pages/home.js
@@ -11,12 +11,13 @@ const Home = {
     return `
       <header class="hero">
         <picture>
-        
         <source media="(max-width: 480px)" srcset="./images/heros/hero-image_2-m.webp" class="hero-img" alt="hero image" type="image/webp">
-        
+        <source media="(max-width: 960px)" srcset="./images/heros/hero-image_2-m.jpg" class="hero-img" alt="hero image" type="image/jpeg">
         <source media="(max-width: 480px)" srcset="./images/heros/hero-image_2-s.webp" class="hero-img" alt="hero image" type="image/webp">
+        <source media="(max-width: 480px)" srcset="./images/heros/hero-image_2-s.jpg" class="hero-img" alt="hero image" type="image/jpeg">
+        <img src="./images/heros/hero-image_2.webp" class="hero-img" alt="hero image" type="image/webp">
+        <img src="./images/heros/hero-image_2.jpg" class="hero-img" alt="hero image" type="image/jpg">
         
-        <source srcset="./images/heros/hero-image_2.webp" class="hero-img" alt="hero image" type="image/webp">
         </picture>
         <div class="hero-container container">
             <h1 class="hero-title">Discover the best restaurant for you</h1>
